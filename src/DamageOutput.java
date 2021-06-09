a0d08b94e4e280b4935103b9afe0caa6121bdd8b
Ziming Qu <82896660+ZyaireSchool@users.noreply.github.com>
2021-06-09 14:09:09 +0000

    Finish the constructors

--- src/DamageOutput.java
+++ src/DamageOutput.java
@@ -1,3 +1,23 @@
 public class DamageOutput{
-  
+  private int dmgOut;
+  private int dmgFinal;
+  private int use;
+
+  public DamageOutPut(int theDmgOut, int theUse){
+    this.dmgOut = theDmgOut;
+    this.use = theUse;
+    this.dmgFinal = this.dmgOut * this.use;
+  }
+
+  public int getDmgOut(){
+    return this.dmgOut;
+  }
+
+  public int getUse(){
+    return this.use;
+  }
+
+  public int getDmgFinal(){
+    return this.dmgFinal;
+  }
 }
